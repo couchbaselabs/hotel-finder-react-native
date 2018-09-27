20be9096654327cf2d56613cede6a60eeaf6c620
James Nocentini <james.nocentini@gmail.com>
2018-09-27 06:43:38 +0100

    configure to use react-native-navigation

--- modules/ROOT/examples/HotelFinder/android/app/src/main/java/com/hotelfinder/MainActivity.java
+++ modules/ROOT/examples/HotelFinder/android/app/src/main/java/com/hotelfinder/MainActivity.java
@@ -1,15 +1,18 @@
 package com.hotelfinder;
 
-import com.facebook.react.ReactActivity;
+import android.graphics.drawable.Drawable;
+import android.support.v4.content.ContextCompat;
+import android.widget.LinearLayout;
 
-public class MainActivity extends ReactActivity {
+import com.reactnativenavigation.controllers.SplashActivity;
 
-    /**
-     * Returns the name of the main component registered from JavaScript.
-     * This is used to schedule rendering of the component.
-     */
+public class MainActivity extends SplashActivity {
     @Override
-    protected String getMainComponentName() {
-        return "HotelFinder";
+    public LinearLayout createSplashLayout() {
+        LinearLayout splash = new LinearLayout(this);
+        Drawable launch_screen_bitmap = ContextCompat.getDrawable(getApplicationContext(),R.drawable.launch_screen_bitmap);
+        splash.setBackground(launch_screen_bitmap);
+
+        return splash;
     }
 }
