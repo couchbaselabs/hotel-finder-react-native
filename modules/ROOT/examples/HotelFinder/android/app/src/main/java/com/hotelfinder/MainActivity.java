fbd4b354fd79fda45394464a80888e71f3a4d30c
James Nocentini <james.nocentini@gmail.com>
2018-10-01 18:36:37 +0100

    ask for overlay permission, document it in tutorial

--- modules/ROOT/examples/HotelFinder/android/app/src/main/java/com/hotelfinder/MainActivity.java
+++ modules/ROOT/examples/HotelFinder/android/app/src/main/java/com/hotelfinder/MainActivity.java
@@ -1,14 +1,53 @@
 package com.hotelfinder;
 
+import android.annotation.TargetApi;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Build;
+import android.os.Bundle;
+import android.provider.Settings;
+import android.support.annotation.Nullable;
 import android.view.View;
 
 import com.reactnativenavigation.controllers.SplashActivity;
 
 public class MainActivity extends SplashActivity {
 
+    public static int ACTION_MANAGE_OVERLAY_PERMISSION_REQUEST_CODE = 5469;
+
     @Override
     public View createSplashLayout() {
         return super.createSplashLayout();
+    }
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        checkPermission();
+    }
+
+    @TargetApi(Build.VERSION_CODES.M)
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == ACTION_MANAGE_OVERLAY_PERMISSION_REQUEST_CODE) {
+            if (!Settings.canDrawOverlays(this)) {
+                // We don't have permission, ask for permission.
+                checkPermission();
+            }
+        }
+
+    }
+
+    public void checkPermission() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            if (!Settings.canDrawOverlays(this)) {
+                Intent intent = new Intent(Settings.ACTION_MANAGE_OVERLAY_PERMISSION,
+                    Uri.parse("package:" + getPackageName()));
+                startActivityForResult(intent, ACTION_MANAGE_OVERLAY_PERMISSION_REQUEST_CODE);
+            }
+        }
     }
 
 }
