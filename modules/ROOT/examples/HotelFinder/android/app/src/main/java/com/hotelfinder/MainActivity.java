0928085cc8f9bf75fc9451458514e0cf0455965d
James Nocentini <james.nocentini@gmail.com>
2018-10-02 16:53:49 +0100

    remove Android permission code.

--- modules/ROOT/examples/HotelFinder/android/app/src/main/java/com/hotelfinder/MainActivity.java
+++ modules/ROOT/examples/HotelFinder/android/app/src/main/java/com/hotelfinder/MainActivity.java
@@ -13,41 +13,9 @@ import com.reactnativenavigation.controllers.SplashActivity;
 
 public class MainActivity extends SplashActivity {
 
-    public static int ACTION_MANAGE_OVERLAY_PERMISSION_REQUEST_CODE = 5469;
-
     @Override
     public View createSplashLayout() {
         return super.createSplashLayout();
-    }
-
-    @Override
-    protected void onCreate(@Nullable Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        checkPermission();
-    }
-
-    @TargetApi(Build.VERSION_CODES.M)
-    @Override
-    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        super.onActivityResult(requestCode, resultCode, data);
-
-        if (requestCode == ACTION_MANAGE_OVERLAY_PERMISSION_REQUEST_CODE) {
-            if (!Settings.canDrawOverlays(this)) {
-                // We don't have permission, ask for permission.
-                checkPermission();
-            }
-        }
-
-    }
-
-    public void checkPermission() {
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
-            if (!Settings.canDrawOverlays(this)) {
-                Intent intent = new Intent(Settings.ACTION_MANAGE_OVERLAY_PERMISSION,
-                    Uri.parse("package:" + getPackageName()));
-                startActivityForResult(intent, ACTION_MANAGE_OVERLAY_PERMISSION_REQUEST_CODE);
-            }
-        }
     }
 
 }
